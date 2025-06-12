c8c611713307629a5c739e0c2187c62fcbb0eafc
5-23 <yhanbyeol6@gmail.com>
2025-06-12 12:34:38 +0900

    fix: add some

--- astro.config.mjs
+++ astro.config.mjs
@@ -28,7 +28,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 export default defineConfig({
   site: "https://blog.asta.rs/",
   base: "/",
-  trailingSlash: "always",
+  trailingSlash: "ignore",
   integrations: [
     tailwind({
       nesting: true,
